e2dda213be96af90f0481d06f4f77a52023bbaf7
ranwenbo <ranwenbo@szzhilai.cn>
2022-07-19 17:51:26 +0800

    add library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import Cookies from "js-cookie";
+
+import ElementPlus from "element-plus";
+import locale from "element-plus/lib/locale/lang/zh-cn"; // 中文语言
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(ElementPlus, {
+  locale: locale,
+  // 支持 large、default、small
+  size: Cookies.get("size") || "default"
+});
+
+app.mount("#app");
